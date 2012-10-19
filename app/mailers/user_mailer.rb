1a66b36884d7f60139decd6236720253116ed448
Mashell <mashell@server.com>
2012-10-19 14:52:48 +0100

    putting in mailgun

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,9 +7,8 @@ class UserMailer < ActionMailer::Base
   #   en.user_mailer.reset_password_email.subject
   #
   def reset_password_email(user)
-    @user = user
     @url  = "http://book-reviewer.herokuapp.com/password_resets/#{user.reset_password_token}/edit"
     mail(:to => user.email,
-         :subject => "Your password has been reset")
-  end
+          :subject => "Your password has been reset")
+    end
 end
