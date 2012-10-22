605dc36343427698727c5eac1125ffbec714dd0c
Mashell <mashell@server.com>
2012-10-22 14:34:44 +0100

    made some actions unavailable to users that haven't logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   
-  skip_before_filter :require_login, :except  => [:destroy]
+  skip_before_filter :require_login
   
   def new
   end
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     if user
       redirect_back_or_to root_url, :notice => "Logged in!"
     else
-      flash.now.alert = "Email or password was invalid"
+      flash.now[:alert] = "Email or password was invalid"
       render :new
     end
   end
